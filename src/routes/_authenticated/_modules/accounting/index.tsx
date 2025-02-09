88a6992a925b6bf15309580d95186de0e507d231
dracu-lah <nevilnicks4321@gmail.com>
2025-02-09 21:15:39 +0530

    some changes

--- src/routes/_authenticated/_modules/accounting/index.tsx
+++ src/routes/_authenticated/_modules/accounting/index.tsx
@@ -2,19 +2,14 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { Sidebar } from "./-components/layout/Sidebar";
 
 export const Route = createFileRoute("/_authenticated/_modules/accounting/")({
-  component: RouteComponent,
+  component: () => {
+    return (
+      <div className="min-h-screen flex flex-col bg-gray-100">
+        <main className="flex-grow p-6">
+          <Outlet />
+        </main>
+        <Sidebar />
+      </div>
+    );
+  },
 });
-
-export const hello = "hsadf";
-function RouteComponent() {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
-      <main className="flex-grow p-6">
-        <Outlet />
-      </main>
-      <Sidebar />
-    </div>
-  );
-}
-
-export default RouteComponent;
