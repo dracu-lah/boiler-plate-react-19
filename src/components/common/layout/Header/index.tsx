87aee13d6d196ddcd53640f5d25968b86433cfcb
dracu-lah <nevilnicks4321@gmail.com>
2025-02-09 23:59:53 +0530

    added some styles

--- src/components/common/layout/Header/index.tsx
+++ src/components/common/layout/Header/index.tsx
@@ -23,29 +23,31 @@ export const Header = () => {
           ProductERP
         </h1>
 
-        {token && (
-          <div className="flex items-center gap-6">
-            {IS_TESTING && (
-              <div className="px-3 py-1 bg-amber-100 text-amber-800 rounded-full text-sm font-medium dark:bg-amber-900 dark:text-amber-200">
-                Test Instance
-              </div>
-            )}
+        <div className="flex  gap-x-10">
+          {token && (
+            <div className="flex items-center gap-6">
+              {IS_TESTING && (
+                <div className="px-3 py-1 bg-amber-100 text-amber-800 rounded-full text-sm font-medium dark:bg-amber-900 dark:text-amber-200">
+                  Test Instance
+                </div>
+              )}
 
-            <div className="flex items-center gap-4">
-              <span className="text-gray-600 dark:text-gray-300">
-                Logged In
-              </span>
-              <button
-                onClick={handleLogout}
-                className="flex items-center gap-2 px-4 py-2 text-sm font-medium text-gray-700 hover:text-gray-900 bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors dark:text-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                <LogOut className="h-4 w-4" />
-                Log out
-              </button>
+              <div className="flex items-center gap-4">
+                <span className="text-gray-600 dark:text-gray-300">
+                  Logged In
+                </span>
+                <button
+                  onClick={handleLogout}
+                  className="flex items-center gap-2 px-4 py-2 text-sm font-medium text-gray-700 hover:text-gray-900 bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors dark:text-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  <LogOut className="h-4 w-4" />
+                  Log out
+                </button>
+              </div>
             </div>
-          </div>
-        )}
-        <ThemeToggle />
+          )}
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
