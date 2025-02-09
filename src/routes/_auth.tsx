53d02ef828031a8311af3c71b0b67ad84f117fc0
dracu-lah <nevilnicks4321@gmail.com>
2025-02-09 13:56:30 +0530

    added redirect to modules

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute("/_auth")({
   beforeLoad: async ({ context }) => {
     const { token } = context.authentication;
     if (token) {
-      throw redirect({ to: "/dashboard" });
+      throw redirect({ to: "/modules" });
     }
   },
 
