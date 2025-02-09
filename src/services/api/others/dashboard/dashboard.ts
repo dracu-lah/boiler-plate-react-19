8349bc151a09be273d5cbe962284b22348550d93
dracu-lah <nevilnicks4321@gmail.com>
2025-02-09 19:57:41 +0530

    fixed dashboard

--- src/services/api/others/dashboard/dashboard.ts
+++ src/services/api/others/dashboard/dashboard.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
-import endPoint from "../../endPoint";
 
 export default {
   get: async (params = {}) => {
-    const { data } = await axios.get(endPoint.dashboard, {
-      params: params,
+    const { data } = await axios.get(`https://dummyjson.com/products`, {
+      params,
     });
     return data;
   },
