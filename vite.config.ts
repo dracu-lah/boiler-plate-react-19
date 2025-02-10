341b2fd0603969852068fdde34c11d81a23c8e99
dracu-lah <nevilnicks4321@gmail.com>
2025-02-10 13:50:30 +0530

    updated vite config files

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,10 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
       "@/modules": path.resolve(
         __dirname,
-        "src/routes/_authenticated/modules/",
+        "./src/routes/_authenticated/modules/",
       ),
     },
   },
