398e2bd975b864f0ceb96bfc8d8078d2de44da00
dracu-lah <nevilnicks4321@gmail.com>
2025-02-10 13:21:52 +0530

    fixed tsc errors

--- src/routes/_authenticated/_modules/accounting/-api/index.ts
+++ src/routes/_authenticated/_modules/accounting/-api/index.ts
@@ -1,5 +1,4 @@
 import { axios } from "@/configs/axios";
-import endpoints from "./endpoints";
 const ledger = {
   get: async (params = {}) => {
     const { data } = await axios.get(`https://dummyjson.com/products`, {
