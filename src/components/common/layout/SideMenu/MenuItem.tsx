586b6aa0cee0644e43d79fa0c9216ebf8cfe7eb3
dracu-lah <nevilnicks4321@gmail.com>
2025-02-11 14:54:45 +0530

    fixed types

--- src/components/common/layout/SideMenu/MenuItem.tsx
+++ src/components/common/layout/SideMenu/MenuItem.tsx
@@ -5,18 +5,26 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import type { MenuItem, SubMenuItem } from "./types";
+import { LucideIcon } from "lucide-react";
+
+interface SubMenuItem {
+  label: string;
+  route: string;
+  icon?: LucideIcon;
+  subMenu?: SubMenuItem[];
+}
+
+interface MenuItem {
+  icon: LucideIcon;
+  label: string;
+  route?: string;
+  subMenu?: SubMenuItem[];
+}
 
 interface SubMenuAccordionProps {
   item: MenuItem | SubMenuItem;
   index: number;
   isItemActive: (item: MenuItem | SubMenuItem) => boolean;
-  subMenu?: SubMenuAccordionProps;
-}
-
-interface MenuItemProps {
-  item: MenuItem;
-  index: number;
 }
 
 const SubMenuAccordion: React.FC<SubMenuAccordionProps> = ({
@@ -24,62 +32,64 @@ const SubMenuAccordion: React.FC<SubMenuAccordionProps> = ({
   index,
   isItemActive,
 }) => {
-  if (!item.subMenu?.subMenu) {
-    return (
-      <Accordion key={index} type="single" collapsible>
-        <AccordionItem
-          className="gap-x-2 rounded-lg border-none duration-300 hover:bg-primary/60 data-[state=open]:bg-primary/20"
-          value={`item-${index}`}
-        >
-          <AccordionTrigger className="px-4">
-            <div className="flex w-[200px] gap-x-2">
-              {item.icon && (
-                <item.icon strokeWidth={3} className="size-5 min-w-fit" />
-              )}
-              <span className="truncate font-semibold">{item.label}</span>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent>
-            <ul className="mx-1 flex flex-col space-y-2 rounded-md">
-              {item.subMenu.map((subItem, subIndex) => (
-                <li key={subIndex}>
-                  {subItem.subMenu ? (
-                    <SubMenuAccordion
-                      item={subItem}
-                      index={subIndex}
-                      isItemActive={isItemActive}
-                    />
-                  ) : (
-                    <Link
-                      key={subIndex}
-                      to={subItem.route}
-                      onClick={() => window.scroll(0, 0)}
-                      className={`flex gap-x-2 rounded-lg px-4 py-4 font-semibold duration-300 hover:bg-primary/60 ${
-                        isItemActive(subItem) ? "bg-primary" : ""
-                      }`}
-                    >
-                      {subItem.icon && (
-                        <subItem.icon
-                          strokeWidth={3}
-                          className="size-5 min-w-fit"
-                        />
-                      )}
-                      <span>{subItem.label}</span>
-                    </Link>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    );
-  } else {
-    return (
-      <SubMenuAccordion item={item} index={index} isItemActive={isItemActive} />
-    );
+  if (!item.subMenu?.length) {
+    return null;
   }
+
+  return (
+    <Accordion key={index} type="single" collapsible>
+      <AccordionItem
+        className="gap-x-2 rounded-lg border-none duration-300 hover:bg-primary/60 data-[state=open]:bg-primary/20"
+        value={`item-${index}`}
+      >
+        <AccordionTrigger className="px-4">
+          <div className="flex w-[200px] gap-x-2">
+            {item.icon && (
+              <item.icon strokeWidth={3} className="size-5 min-w-fit" />
+            )}
+            <span className="truncate font-semibold">{item.label}</span>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent>
+          <ul className="mx-1 flex flex-col space-y-2 rounded-md">
+            {item.subMenu.map((subItem, subIndex) => (
+              <li key={subIndex}>
+                {subItem.subMenu ? (
+                  <SubMenuAccordion
+                    item={subItem}
+                    index={subIndex}
+                    isItemActive={isItemActive}
+                  />
+                ) : (
+                  <Link
+                    to={subItem.route}
+                    onClick={() => window.scroll(0, 0)}
+                    className={`flex gap-x-2 rounded-lg px-4 py-4 font-semibold duration-300 hover:bg-primary/60 ${
+                      isItemActive(subItem) ? "bg-primary" : ""
+                    }`}
+                  >
+                    {subItem.icon && (
+                      <subItem.icon
+                        strokeWidth={3}
+                        className="size-5 min-w-fit"
+                      />
+                    )}
+                    <span>{subItem.label}</span>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
 };
+
+interface MenuItemProps {
+  item: MenuItem;
+  index: number;
+}
 
 const MenuItem: React.FC<MenuItemProps> = ({ item, index }) => {
   const location = useLocation();
@@ -89,7 +99,6 @@ const MenuItem: React.FC<MenuItemProps> = ({ item, index }) => {
     const isCurrentSubRoute = item.route
       ? location.pathname.startsWith(item.route)
       : false;
-
     return isCurrentRoute || isCurrentSubRoute;
   };
 
@@ -97,21 +106,20 @@ const MenuItem: React.FC<MenuItemProps> = ({ item, index }) => {
     return (
       <SubMenuAccordion item={item} index={index} isItemActive={isItemActive} />
     );
-  } else {
-    return (
-      <Link
-        key={index}
-        to={item.route!}
-        onClick={() => window.scroll(0, 0)}
-        className={`flex min-w-fit gap-x-2 rounded-lg px-4 py-4 font-semibold duration-300 hover:bg-primary/60 ${
-          isItemActive(item) ? "bg-primary" : ""
-        }`}
-      >
-        {item.icon && <item.icon strokeWidth={3} className="size-5" />}
-        <span>{item.label}</span>
-      </Link>
-    );
   }
+
+  return (
+    <Link
+      to={item.route!}
+      onClick={() => window.scroll(0, 0)}
+      className={`flex min-w-fit gap-x-2 rounded-lg px-4 py-4 font-semibold duration-300 hover:bg-primary/60 ${
+        isItemActive(item) ? "bg-primary" : ""
+      }`}
+    >
+      {item.icon && <item.icon strokeWidth={3} className="size-5" />}
+      <span>{item.label}</span>
+    </Link>
+  );
 };
 
 export default MenuItem;
--- src/components/common/layout/SideMenu/types.ts
+++ src/components/common/layout/SideMenu/types.ts
@@ -1,17 +1,3 @@
 import { RegisteredRouter, RouteIds, RoutePaths } from "@tanstack/react-router";
-import { LucideIcon } from "lucide-react";
 export type RoutePath = RoutePaths<RegisteredRouter["routeTree"]>;
 export type RouteId = RouteIds<RegisteredRouter["routeTree"]>;
-export interface SubMenuItem {
-  label: string;
-  route: RoutePath;
-  icon?: LucideIcon;
-  subMenu?: SubMenuItem[];
-}
-
-export interface MenuItem {
-  icon: LucideIcon;
-  label: string;
-  route?: RoutePath;
-  subMenu?: SubMenuItem[];
-}
