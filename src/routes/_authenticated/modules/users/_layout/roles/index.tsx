55244cb21178144d2547a8991076fdf5902fcc16
dracu-lah <nevilnicks4321@gmail.com>
2025-02-20 16:48:54 +0530

    fixed some imports

--- src/routes/_authenticated/modules/users/_layout/roles/index.tsx
+++ src/routes/_authenticated/modules/users/_layout/roles/index.tsx
@@ -1,15 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute(
-  '/_authenticated/modules/users/_layout/roles/',
+  "/_authenticated/modules/users/_layout/roles/",
 )({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
     <div>
       Hello "/_authenticated/modules/accounting/_accountingLayout/roles/"!
     </div>
-  )
+  );
 }
