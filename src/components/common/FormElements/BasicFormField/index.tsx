0fa60c0f036fc32a923ec7d94589902aa243cf8c
dracu-lah <nevilnicks4321@gmail.com>
2025-02-19 17:45:38 -0800

    new changes

--- src/components/common/FormElements/BasicFormField/index.tsx
+++ src/components/common/FormElements/BasicFormField/index.tsx
@@ -6,33 +6,47 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useFormContext } from "react-hook-form"; // Import useFormContext
+import { useFormContext } from "react-hook-form";
 
-export default function   BasicFormField({
+interface BasicFormFieldProps {
+  name: string;
+  placeholder?: string;
+  label?: string;
+  required?: boolean;
+  type?: string;
+  className?: string;
+  disabled?: boolean;
+}
+
+export default function BasicFormField({
   name,
-  placeholder,
+  placeholder = "",
   label,
-  required,
-  type,
-  className,
+  required = false,
+  type = "text",
+  className = "",
   disabled = false,
-}) {
+}: BasicFormFieldProps) {
   const { control } = useFormContext();
+
   return (
     <FormField
-      disabled={disabled}
-      key={name}
       control={control}
       name={name}
       render={({ field }) => (
         <FormItem className="flex flex-col">
-          {label && <FormLabel>{label} {required && <span className="text-red-500">*</span>}</FormLabel>}
+          {label && (
+            <FormLabel>
+              {label} {required && <span className="text-red-500">*</span>}
+            </FormLabel>
+          )}
           <FormControl>
             <Input
               type={type}
               placeholder={placeholder}
               {...field}
               className={className}
+              disabled={disabled}
             />
           </FormControl>
           <FormMessage />
